09703dbffc4c452e5a267c5426dc0f57b9700306
ekecardinal <ekecardinal@gmail.com>
2024-11-21 12:46:58 +0100

    fixed task description

--- src/features/tasks/components/task-description.tsx
+++ src/features/tasks/components/task-description.tsx
@@ -1,40 +1,42 @@
-// import { useState } from "react";
-// import { PencilIcon, XIcon } from "lucide-react";
+import { useState } from "react";
+import { PencilIcon, XIcon } from "lucide-react";
 
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-// import { DottedSeparator } from "@/components/dotted-separator";
+import { DottedSeparator } from "@/components/dotted-separator";
 
-// import { Task } from "../types";
-// import { useUpdateTask } from "../api/use-update-task";
+import { Task } from "../types";
+import { useUpdateTask } from "../api/use-update-task";
 
 interface TaskDescriptionProps {
-  desc: string;
-  setDesc: (desc: string) => void;
+  task: Task;
 }
 
-export const TaskDescription = ({ desc, setDesc }: TaskDescriptionProps) => {
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [value, setValue] = useState(task.description);
+export const TaskDescription = ({ task }: TaskDescriptionProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(task.description);
 
-  // const { mutate, isPending } = useUpdateTask();
+  const { mutate, isPending } = useUpdateTask();
 
-  // const handleSave = () => {
-  //   mutate({
-  //     json: { description: value },
-  //     param: { taskId: task.$id }
-  //   }, {
-  //     onSuccess: () => {
-  //       setIsEditing(false);
-  //     }
-  //   });
-  // };
+  const handleSave = () => {
+    mutate(
+      {
+        json: { description: value },
+        param: { taskId: task.$id },
+      },
+      {
+        onSuccess: () => {
+          setIsEditing(false);
+        },
+      }
+    );
+  };
 
   return (
     <div className="p-4 border rounded-lg">
-      {/* <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <p className="text-lg font-semibold">Overview</p>
-        <div
+        <Button
           onClick={() => setIsEditing((prev) => !prev)}
           size="sm"
           variant="secondary"
@@ -45,34 +47,34 @@ export const TaskDescription = ({ desc, setDesc }: TaskDescriptionProps) => {
             <PencilIcon className="size-4 mr-2" />
           )}
           {isEditing ? "Cancel" : "Edit"}
-        </div>
-      </div> */}
-      {/* <DottedSeparator className="my-4" />
-      {isEditing ? ( */}
-      <div className="flex flex-col gap-y-4">
-        <Textarea
-          placeholder="Add a description..."
-          value={desc}
-          rows={4}
-          onChange={(e) => setDesc(e.target.value)}
-          // disabled={isPending}
-        />
-        {/* <Button
+        </Button>
+      </div>
+      <DottedSeparator className="my-4" />
+      {isEditing ? (
+        <div className="flex flex-col gap-y-4">
+          <Textarea
+            placeholder="Add a description..."
+            value={value}
+            rows={4}
+            onChange={(e) => setValue(e.target.value)}
+            disabled={isPending}
+          />
+          <Button
             size="sm"
             className="w-fit ml-auto"
             onClick={handleSave}
             disabled={isPending}
           >
             {isPending ? "Saving..." : "Save Changes"}
-          </Button> */}
-      </div>
-      {/* ) : (
+          </Button>
+        </div>
+      ) : (
         <div>
-          {desc || (
+          {task.description || (
             <span className="text-muted-foreground">No description set</span>
           )}
         </div>
-      )} */}
+      )}
     </div>
   );
 };
