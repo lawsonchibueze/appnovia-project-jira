e755955f0313361ccdb25501e1b0e01cbadaadd9
Lawson Chibueze <speak2lawson@gmail.com>
2024-11-29 15:26:00 +0100

    "updated home Page"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PenBox } from "lucide-react";
+import { Handshake } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
@@ -21,9 +26,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cn(inter.className, "antialiased min-h-screen")}
-      >
+      <body className={cn(inter.className, "antialiased min-h-screen")}>
+        <nav className="flex justify-between items-center">
+          <Image src="/logo5.webp" alt="logo" width={152} height={56} />
+          <div className="flex justify-end">
+            <Link href="https://novia-teams.vercel.app">
+              <Button className="flex items-center gap-2 border-solid border-2 border-white bg-black text-white hover:text-black ">
+                <Handshake size={18} />
+                <span className="hidden md:inline">noviaTeams</span>
+              </Button>
+            </Link>
+            <Link href="https://project-mgnt-j-25rf.vercel.app/">
+              <Button className="flex items-center gap-2">
+                <PenBox size={18} />
+                <span className="hidden md:inline">noviaPlanner</span>
+              </Button>
+            </Link>
+          </div>
+        </nav>
         <QueryProvider>
           <Toaster />
           {children}
